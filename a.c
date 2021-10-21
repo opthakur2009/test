993900fa76d1b32d2b2ed80de2ddcad3d4998783
opthakur2009 <88101604+opthakur2009@users.noreply.github.com>
2021-10-21 13:32:53 +0530

    Add files via upload

---
+++ a.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main()
+{ int a[][3]={{1,2,3},{4,5,6},{7,8,9}},i,j;
+ for(i=0;i<3;i++)
+   {printf("\n");
+   	for(j=0;j<3;++j)
+	{
+	printf("%d", a[i][j] );
+     }
+   }
+
+
+}
